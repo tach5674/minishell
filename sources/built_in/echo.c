4d2f8c354ffae21acc2cabc25c6682a42115a874
Glak Gevorgian <ggevorgi@c2r11s6.42yerevan.am>
2025-06-14 13:47:46 +0400

    fix echo -n

--- sources/built_in/echo.c
+++ sources/built_in/echo.c
@@ -3,47 +3,46 @@
 /*                                                        :::      ::::::::   */
 /*   echo.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mzohraby <mzohraby@student.42.fr>          +#+  +:+       +#+        */
+/*   By: ggevorgi <sp1tak.gg@gmail.com>             +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 17:17:06 by ggevorgi          #+#    #+#             */
-/*   Updated: 2025/06/12 17:36:22 by mzohraby         ###   ########.fr       */
+/*   Updated: 2025/06/14 13:40:39 by ggevorgi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static bool	has_n_flag(char *s)
+static bool	is_valid_n_flag(const char *s)
 {
-	if (!s || !(*s))
+	if (!s || s[0] != '-' || s[1] != 'n')
 		return (false);
-	if (*s != '-')
-		return (false);
-	s++;
-	if (*s != 'n')
-		return (false);
+	s += 2;
 	while (*s == 'n')
-		++s;
-	if (!(*s))
-		return (true);
-	return (false);
+		s++;
+	return (*s == '\0');
 }
 
 int	ft_echo(t_cmd *cmd)
 {
-	bool	has_newline;
 	int		i;
+	bool	has_newline;
 
 	i = 1;
-	has_newline = has_n_flag(cmd->args[i]);
-	if (has_newline)
-		++i;
-	if (cmd->args[i])
+	has_newline = true;
+	while (cmd->args[i] && is_valid_n_flag(cmd->args[i]))
+	{
+		has_newline = false;
+		i++;
+	}
+	while (cmd->args[i])
+	{
 		if (printf("%s", cmd->args[i++]) == -1)
 			return (handle_error("echo: write error"));
-	while (cmd->args[i])
-		if (printf(" %s", cmd->args[i++]) == -1)
-			return (handle_error("echo: write error"));
-	if (!has_newline)
+		if (cmd->args[i])
+			if (printf(" ") == -1)
+				return (handle_error("echo: write error"));
+	}
+	if (has_newline)
 		if (printf("\n") == -1)
 			return (handle_error("echo: write error"));
 	return (0);
